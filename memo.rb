b27a045e6d1b9d1f2ec070ce5b4814ab38d9d128
t19106 <g037sk@gmail.com>
2023-01-16 00:41:54 +0900

    first commit

---
+++ memo.rb
@@ -0,0 +1,56 @@
+require "csv"
+
+puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+puts "3 → 既存メモを閲覧する"
+memo_type = gets.to_i
+
+if memo_type == 1
+    puts "拡張子を除いたファイルを入力してください"
+    name = gets.chomp
+    puts "メモしたい内容を入力してください"
+    input = readlines
+    CSV.open("#{name}.csv",'w') do |test|
+        test << input
+    end
+    puts "下記の内容を保存しました："
+    puts "----------"
+    CSV.foreach("#{name}.csv") do |row|
+        puts row
+    end
+elsif memo_type == 2
+    puts "拡張子を除いたファイルを入力してください"
+    name = gets.chomp
+    puts "編集方法を入力してください"
+    puts "1 → メモの内容を全て上書きする / 2 → メモに追記する"
+    puts "3 → プログラムを終了する"
+    puts "注意： 上書き・追尾後はやり直せません"
+    edit_type = gets.to_i
+    if edit_type == 1
+        puts "メモしたい内容を入力してください"
+        input = readlines
+        CSV.open("#{name}.csv",'w') do |test|
+            test << input
+        end
+    elsif edit_type == 2
+        puts "メモしたい内容を入力してください"
+        input = readlines
+        CSV.open("#{name}.csv",'a') do |test|
+            test << input
+        end
+    end
+    puts "下記の内容を保存しました："
+    puts "----------"
+    CSV.foreach("#{name}.csv") do |row|
+        puts row
+    end
+elsif memo_type == 3
+    puts "拡張子を除いたファイルを入力してください"
+    name = gets.chomp
+    puts "メモの内容を表示します："
+    puts "----------"
+    CSV.foreach("#{name}.csv") do |row|
+        puts row
+    end
+end
+puts "----------"
+puts "アプリを終了します。"
